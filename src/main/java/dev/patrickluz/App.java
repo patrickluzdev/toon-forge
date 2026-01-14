5f69c5b00b65419417c1677360541455a6c7ab38
Patrick Luz <eu@patrickluz.dev>
2026-01-13 21:12:15 -0300

    chore: position window on secondary monitor

--- src/main/java/dev/patrickluz/App.java
+++ src/main/java/dev/patrickluz/App.java
@@ -3,10 +3,12 @@ package dev.patrickluz;
 import dev.patrickluz.core.Renderer;
 import javafx.animation.AnimationTimer;
 import javafx.application.Application;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Scene;
 import javafx.scene.image.ImageView;
 import javafx.scene.image.WritableImage;
 import javafx.scene.layout.StackPane;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 import java.awt.Color;
@@ -18,9 +20,17 @@ public class App extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         Renderer renderer = new Renderer(600, 400);
-
         WritableImage preview = SwingFXUtils.toFXImage(renderer.getFrame(), null);
         ImageView imageView = new ImageView(preview);
+
+        var screens = Screen.getScreens();
+        Screen targetScreen = screens.size() > 1 ? screens.get(1) : screens.get(0);
+        Rectangle2D bounds = targetScreen.getVisualBounds();
+
+        int windowWidth = renderer.getFrameWidth();
+        int padding = 30;
+        stage.setX(bounds.getMaxX() - windowWidth - padding);
+        stage.setY(bounds.getMinY() + padding);
 
         StackPane root = new StackPane(imageView);
         Scene scene = new Scene(root, renderer.getFrameWidth(), renderer.getFrameHeight());
